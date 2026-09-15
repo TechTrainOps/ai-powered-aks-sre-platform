54418c4132df44f8e70014d893dd19d4101ba2a2
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-16 02:12:37 +0530

    added stg for incident collection

--- infra/main.tf
+++ infra/main.tf
@@ -236,7 +236,7 @@ resource "azurerm_storage_account" "incident_storage" {
   name                     = "staiakssreinc2026"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 
   min_tls_version = "TLS1_2"
