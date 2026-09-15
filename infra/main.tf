81ce64342fbe6a7b9f88ca8bc198d04ce0661293
Shakir Ahmed Shah <shakirshahahmed@gmail.com>
2026-09-16 02:23:54 +0530

    added stg for incident collection

--- infra/main.tf
+++ infra/main.tf
@@ -239,7 +239,8 @@ resource "azurerm_storage_account" "incident_storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  min_tls_version = "TLS1_2"
+  min_tls_version           = "TLS1_2"
+  shared_access_key_enabled = false
 
   allow_nested_items_to_be_public = false
 
